13eeee5062b481d7e7a0ecabf41b32b9dc696d53
AdityasagarR123 <adisagar450@gmail.com>
2026-06-12 15:54:01 +0530

    Create linear_regression.py

---
+++ linear_regression.py
@@ -0,0 +1,13 @@
+Linear Regression in Machine learning
+Last Updated : 4 Jun, 2026
+Linear Regression is a fundamental supervised learning algorithm used to model the relationship between a dependent variable and one or more independent variables. It predicts continuous values by fitting a straight line that best represents the data.
+
+It assumes that there is a linear relationship between the input and output
+Uses a best‑fit line to make predictions
+Commonly used in forecasting, trend analysis, and predictive modelling
+how_does_linear_regression_work_.webphow_does_linear_regression_work_.webp
+For example, suppose we want to predict a student’s exam score based on the number of hours studied. As study hours increase, exam scores generally increase as well. Here:
+
+Independent variable (input): Hours studied because it's the factor we control or observe.
+Dependent variable (output): Exam score because it depends on how many hours were studied.
+Linear regression uses the independent variable to predict the dependent variable.
